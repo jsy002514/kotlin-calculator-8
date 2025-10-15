0365e0408e7be2af90a2f833513b01bc3501b59e
jsy002514 <jsy002514@naver.com>
2025-10-15 12:09:43 +0900

    feat(stringcalculator):각 클래스 호출 및 합 리턴 기능 구현

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -4,10 +4,15 @@ class StringCalculator(
     private val summer: NumberSummer,
     private val extractor: NumberExtractor,
     private val validator: NumberValidator) {
-    fun add(text: String?){
+    fun add(text: String?) : Long{
         //추출
         val extractedstring = extractor.extract(text)
         //추출한 문자열 확인
+        val checkedstring = checkNumbers(extractedstring)
+        //숫자 리스트 규칙에 맞는지 확인
+        val validatednumbers = validator.validate(checkedstring)
+        //숫자 더하기
+        return summer.sum(validatednumbers)
     }
     private fun checkNumbers(strings: List<String>):List<Long>{
         return strings
