478d164a5e4d05d7909437e3eb16ff1b4c744b89
jsy002514 <jsy002514@naver.com>
2025-10-15 12:57:54 +0900

    feat(numberextractor):구분자 패턴으로 문자열 추출 기능 구현

--- src/main/kotlin/calculator/NumberExtractor.kt
+++ src/main/kotlin/calculator/NumberExtractor.kt
@@ -1,4 +1,27 @@
 package calculator
 
+import java.util.regex.Pattern
+
 class NumberExtractor {
+    companion object{
+        private const val CUSTOM_DELIMETER_REGEX = "//(.*)\n(.*)"
+        private val pattern: Pattern = Pattern.compile(CUSTOM_DELIMETER_REGEX)
+    }
+    fun extract(text: String?) : List<String>{
+        if(text.isNullOrBlank()){
+            return emptyList()
+        }
+
+        val delimeter = mutableListOf(",",":")
+        var numberstring = text
+
+        val matcher = pattern.matcher(text)
+        if(matcher.find()){
+            val customdelimeter = matcher.group(1)
+            delimeter.add(customdelimeter)
+            numberstring = matcher.group(2)
+        }
+
+        return numberstring.split(*delimeter.toTypedArray())
+    }
 }
