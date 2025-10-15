6776acdc4792e855eb21889dcc3a10508a5f876f
jsy002514 <jsy002514@naver.com>
2025-10-15 13:44:10 +0900

    feat(numbervalidator):숫자 목록 내 음수 확인 기능 구현

--- src/main/kotlin/calculator/NumberValidator.kt
+++ src/main/kotlin/calculator/NumberValidator.kt
@@ -1,4 +1,10 @@
 package calculator
 
 class NumberValidator {
+    fun validate(numbers: List<Long>){
+        val negativenumbers = numbers.filter { it < 0 }
+        if(negativenumbers.isNotEmpty()){
+            throw IllegalArgumentException("음수는 허용되지 않습니다: $negativenumbers")
+        }
+    }
 }
