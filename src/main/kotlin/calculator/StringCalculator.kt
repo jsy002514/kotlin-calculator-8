39c8eea1885421010f88d20ec3def2b14c52e421
jsy002514 <jsy002514@naver.com>
2025-10-15 11:58:08 +0900

    feat(stringcalculator):추출한 문자열 검사 기능 구현

--- src/main/kotlin/calculator/StringCalculator.kt
+++ src/main/kotlin/calculator/StringCalculator.kt
@@ -1,4 +1,23 @@
 package calculator
 
-class StringCalculator {
+class StringCalculator(
+    private val summer: NumberSummer,
+    private val extractor: NumberExtractor,
+    private val validator: NumberValidator) {
+    fun add(text: String?){
+        //추출
+        val extractedstring = extractor.extract(text)
+        //추출한 문자열 확인
+    }
+    private fun checkNumbers(strings: List<String>):List<Long>{
+        return strings
+            .filter { it.isNotEmpty() }
+            .map { str ->
+                try{
+                    str.toLong()
+                }catch (e: NumberFormatException){
+                    throw IllegalArgumentException("숫자 형식에 맞지 않거나 너무 큰 숫자입니다: $str")
+                }
+            }
+    }
 }
