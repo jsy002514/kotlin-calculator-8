3ac036d821218ea471a9dac1d49d59727397c1e9
jsy002514 <jsy002514@naver.com>
2025-10-15 11:43:27 +0900

    feat(application):계산 및 결과 출력 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -7,5 +7,17 @@ fun main() {
     if(userinput == null){
         return
     }
-
+    //계산기 객체를 생성한다
+    val calculator = StringCalculator(
+        NumberSummer(),
+        NumberExtractor(),
+        NumberValidator()
+    )
+    //계산기 객체로부터 결과를 전달받고 알맞은 출력문을 출력한다
+    try{
+        val result = calculator.add(userinput)
+        println("결과: $result")
+    }catch (e : IllegalArgumentException){
+        println("오류: ${e.message}")
+    }
 }
