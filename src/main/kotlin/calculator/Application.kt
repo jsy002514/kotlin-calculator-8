d50a26ceab2b251b3730f65b605eb640069ab230
jsy002514 <jsy002514@naver.com>
2025-10-15 11:39:51 +0900

    feat(application):사용자 입력 및 예외처리 기능 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -1,5 +1,11 @@
 package calculator
 
 fun main() {
-    // TODO: 프로그램 구현
+    //사용자에게 입력을 요청하는 안내문을 출력하고 문자열을 입력받는다
+    println("덧셈할 문자열을 입력해주세요.")
+    val userinput = camp.nextstep.edu.missionutils.Console.readLine()
+    if(userinput == null){
+        return
+    }
+
 }
