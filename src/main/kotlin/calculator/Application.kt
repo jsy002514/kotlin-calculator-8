afbbcbfff342391334081ad36d60fed2d5fabe7a
jsy002514 <jsy002514@naver.com>
2025-10-19 15:18:25 +0900

    fix(application): 결과 출력문 수정

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -17,8 +17,8 @@ fun main() {
     //계산기 객체로부터 결과를 전달받고 알맞은 출력문을 출력한다
     try{
         val result = calculator.add(userinput)
-        println("결과: $result")
+        println("결과 : $result")
     }catch (e : IllegalArgumentException){
-        println("오류: ${e.message}")
+        println("오류 : ${e.message}")
     }
 }
